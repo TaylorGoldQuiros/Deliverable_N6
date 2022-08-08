68e5c2f08ecc6fe77c323c85654474c73c349bb5
feijodelima86 <rafael.feijo.bio.uerj@gmail.com>
2022-08-08 15:29:59 -0600

    Small Fish PCA

--- 1_Protocol/PCA.R
+++ 1_Protocol/PCA.R
@@ -15,15 +15,14 @@ library(car)
 library(ggpubr)
 library(cowplot)
 library(factoextra)
-
+library(readxl)
 #### PCA ####
 
 
 
-CPUE_HIstorical <- read_excel("0_Data/CPUE HIstorical.xlsx")
+CPUE_HIstorical <- read_excel("0_Data/Smallfish_PCA.xlsx")
 
-
-pca9.ln <- prcomp(CPUE_HIstorical[c(9:12)],  scale = TRUE)
+pca9.ln <- prcomp(CPUE_HIstorical[c(3:8)],  scale = TRUE)
 
 plot(pca9.ln)
 
@@ -34,11 +33,12 @@ aload <- abs(pca9.ln$rotation)
 results <- sweep(aload, 2, colSums(aload), "/")
 dev.new()
 factoextra::fviz_pca_biplot(pca9.ln, 
-                            habillage=CPUE_HIstorical$Site,
+                            habillage=CPUE_HIstorical$AREA,
                             geom.ind = c("point"), 
                             #numbers removed use point only
                             col.var="black", #change colors of arrow variables
-                            addEllipses = TRUE, 
-                            repel = TRUE,
-                            title = NULL) 
-
+#                            addEllipses = TRUE, 
+                            repel = FALSE,
+                            title = NULL) +
+                            xlim(-5, 5) + ylim (-2.5, 2.5)
+ 
